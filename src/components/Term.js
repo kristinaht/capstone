356b57377595dd75621658543a1849299b6e1c38
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-08 13:59:08 -0700

    create basic version of Term.js component

---
+++ src/components/Term.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function Term(props){
+  return(
+    <React.Fragment>
+      <div>
+        <h2>Term Name: Insurance</h2>
+        <h2>Insurance language term language goes here</h2>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Term;
+
+Term.propTypes = {
+  
+}
