631486650a1e6f2ff7ed42cd60419148c8f73014
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-12 18:28:06 -0700

    add non-functioning onClick() method to Term.js

--- src/components/Term.js
+++ src/components/Term.js
@@ -5,16 +5,18 @@ import PropTypes from 'prop-types';
 function Term(props){
   return(
     <React.Fragment>
-      <div>
+      <div onClick = {() => props.whenTermClicked(props.id)}>
         <h2>Term Name: {props.name}</h2>
         <h2>{props.body}</h2>
       </div>
+      <hr/>
     </React.Fragment>
   )
 }
 
-export default Term;
 
 Term.propTypes = {
 
 }
+export default Term;
+
