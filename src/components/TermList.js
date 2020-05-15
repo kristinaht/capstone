96e8af1c120fd6ca3801f2aa63392f3dc38f952a
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-15 11:39:54 -0700

    refactor TermList component to view terms from firestore

--- src/components/TermList.js
+++ src/components/TermList.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import Term from './Term';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 function TermList(props) {
-  return (
-    <React.Fragment>
-      <hr/>
-      { Object.values(props.termList).map((term) => {
-        return <Term
-        whenTermClicked={ props.onTermSelection }
-        name={term.name}
-        body={term.body}
-        id={term.id}
-        key={term.id} />
+  
+  useFirestoreConnect([
+    { collection: 'terms' }
+  ]);
 
-      })}
-    </React.Fragment>
-  );
+  const terms = useSelector(state => state.firestore.ordered.terms);
+
+  if(isLoaded(terms)) {
+    return(
+      <React.Fragment>
+        <hr/>
+        { terms.map((term) => {
+          return <Term 
+            whenTermClicked={ props.onTermSelection }
+            name={ term.name }
+            body={ term.body }
+            id={ term.id }
+            key={ term.id } />
+        })}
+      </React.Fragment>
+    )
+  } else {
+    return(
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
+
 }
 
 export default TermList;
+
+TermList.propTypes = {
+  onTermSelection: PropTypes.func
+}
