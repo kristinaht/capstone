4502ee36e16bde68071a180bfec774a48571e22f
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-16 18:43:13 -0700

    import and use Router, Switch, Route in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,30 @@ import './App.css';
 import Header from "./Header";
 import TermControl from "./TermControl"; 
 import Footer from "./Footer";
+import Signin from './Signin';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <TermControl /> 
+      <Switch>
+        <Route path='/signin'>
+          <Signin />
+        </Route>
+        <Route path='/'>
+          <TermControl />
+        </Route>
+      </Switch>
       <Footer />
-    </React.Fragment>
+    </Router>
   );
 }
 
 export default App;
+
+    // <React.Fragment>
+    //   <Header />
+    //   <TermControl /> 
+    //   <Footer />
+    // </React.Fragment>
