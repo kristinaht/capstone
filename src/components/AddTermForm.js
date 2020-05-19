7502aa04f0ed58de657ac6a16d96061ea89c0faf
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-19 12:06:01 -0700

    modify TermDetail and AddTermForm components

--- src/components/AddTermForm.js
+++ src/components/AddTermForm.js
@@ -5,15 +5,15 @@ import { Form, Button } from 'react-bootstrap';
 
 function AddTermForm(props) {
 
+  const formStyles = {
+    display: 'block',
+    width: '100%',
+    borderRadius: '5px'
+  }
   const firestore = useFirestore();
   
   function addTermToFirestore(event) {
 
-    const formStyles = {
-      display: 'block',
-      width: '100%',
-      borderRadius: '5px'
-    }
 
     event.preventDefault();
     props.onNewTermCreation();
@@ -32,26 +32,26 @@ function AddTermForm(props) {
     <React.Fragment>
       <Form onSubmit={addTermToFirestore}>
         <Form.Group>
-          <Form.Label>Document name:</Form.Label>
-          <Form.Control type='text' name='name' required/>
+          <Form.Label style={formStyles}>Document name:</Form.Label>
+          <Form.Control style={formStyles} type='text' name='name' required/>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Parties:</Form.Label>
-          <Form.Control type='text' name='parties' placeholder='This contract is between...' required/>
+          <Form.Label style={formStyles} >Parties:</Form.Label>
+          <Form.Control style={formStyles} type='text' name='parties' placeholder='This contract is between...' required/>
         </Form.Group>
         <Form.Group>
-          <Form.Label>WHEREAS clause:</Form.Label>
-          <Form.Control type='text' name='whereas' placeholder='WHEREAS...' required/>
+          <Form.Label style={formStyles} >WHEREAS clause:</Form.Label>
+          <Form.Control style={formStyles} type='text' name='whereas' placeholder='WHEREAS...' required/>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Scope of Work:</Form.Label>
-          <Form.Control type='text' name='sow' placeholder='...' required/>
+          <Form.Label style={formStyles}>Scope of Work:</Form.Label>
+          <Form.Control style={formStyles} type='text' name='sow' placeholder='...' required/>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Governing law:</Form.Label>
-          <Form.Control type='text' name='govLaw' placeholder='All matters and actions in this agreement shall be governed by...' required/>
+          <Form.Label style={formStyles}>Governing law:</Form.Label>
+          <Form.Control style={formStyles} type='text' name='govLaw' placeholder='All matters and actions in this agreement shall be governed by...' required/>
         </Form.Group>
-        <Button variant='outline-dark' type='submit'>Add new terms</Button>
+        <Button style={formStyles} variant='outline-dark' type='submit'>Add new terms</Button>
       </Form>
     </React.Fragment>
   )
--- src/components/TermDetail.js
+++ src/components/TermDetail.js
@@ -9,8 +9,23 @@ function TermDetail(props) {
       <h1>Document Detail</h1>
       <h2>{ term.name } </h2>
       <h3>{ term.parties }</h3>
+      <h4>1. Services</h4>
+      <p>Contractor shall perform the services set out in the Statement of Work above (the “Services”) in accordance with the
+      terms and conditions of this Contract and in a professional and workmanlike manner, in accordance with generally recognized industry
+      standards for similar services;</p>
+      <p>1.2 This Contract does not create an employer/employee relationship between the parties. It is the parties’ intention that
+      Contractor will be an independent contractor and not an employee of OHSU for any purpose. Contractor shall be responsible for
+      compliance with all federal, state and local laws, ordinances, regulations and orders that are applicable to this Contract and
+      Contractor’s performance hereunder ( « Applicable Laws ») related to its employees and agents. Contractor will retain sole and
+      absolute discretion over the manner and means of carrying out Contractor’s activities and responsibilities hereunder. </p>
       <p>{ term.whereas }</p>
+      <h4>2. Scope of Work</h4>
       <p>{ term.sow }</p>
+      <h4>3. Responsibility for Damages; Indemnity</h4>
+      <p>11.1 Contractor shall be responsible for all damage to property, injury to persons, and loss, expense, inconvenience, and
+      delay which may be caused by, or result from, the conduct of work under this Contract, or from any act, omission, or neglect of the
+      Contractor, its subcontractors, or employees.</p>
+      <h4>4. Governing Law/Venue</h4>
       <p>{ term.govLaw }</p>
       
       <button onClick={ () => onClickingDelete(term.id) }>Delete </button>
