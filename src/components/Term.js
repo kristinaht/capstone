f8a41b10ba77d18724c9de8ca84247b033a5f10c
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-05-20 11:55:47 -0700

    use Card in Term component

--- src/components/Term.js
+++ src/components/Term.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 
 function Term(props){
+
+  const termStyle = {
+    textAlign: 'center',
+    border: '2px solid rgb(70,108,82)',
+    width: '400px',
+    height: '400px',
+    padding: '2%',
+    margin: '1%',
+    boxShadow: '0 2px 4px 0 rgba(0,0,0,0.10)'
+  }
+
+
   return(
     <React.Fragment>
-      <div onClick = {() => props.whenTermClicked(props.id)}>
+      <Card style={termStyle} onClick = {() => props.whenTermClicked(props.id)}>
         <p>{props.name}</p>
         <p>{props.contractor}</p>
         {/* <button onClick = {()=> props.whenTermClicked(props.id)}>DETAILS</button> */}
-      </div>
+      </Card>
       <hr/>
     </React.Fragment>
   )
